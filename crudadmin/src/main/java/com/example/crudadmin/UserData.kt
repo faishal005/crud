bf326f35e80e4b9065b67a9746ec13d8f1ffaf1c
faishal005 <faishalsilentis@gmail.com>
2023-05-17 22:22:16 +0700

    first commit

---
+++ crudadmin/src/main/java/com/example/crudadmin/UserData.kt
@@ -0,0 +1,5 @@
+package com.example.crudadmin
+
+class UserData (val name : String? = null, val operator : String? = null,
+                val location : String? = null, val phone : String? = null){
+}
